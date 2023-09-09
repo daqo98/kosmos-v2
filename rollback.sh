2e610bbc0d231814667960108f2b819c4da26f31
David Quintero <davidalfredo.quintero@mail.polimi.it>
2023-09-09 13:49:38 +0200

    Modifying rollback order

--- rollback.sh
+++ rollback.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Deleting old resources
-kubectl delete -f config/cluster-conf/e2e-namespace.yaml
-kubectl delete -f "config/crd/bases"
-kubectl delete -f "config/permissions"
-kubectl delete -f "config/storage"
-kubectl delete -f "examples/benchmark/application2"
-kubectl delete -f "examples/benchmark/metrics"
 kubectl delete -f "examples/benchmark/system-autoscaler"
+kubectl delete -f "examples/benchmark/metrics"
+kubectl delete -f "examples/benchmark/application2"
+kubectl delete -f "config/storage"
+kubectl delete -f "config/permissions"
+kubectl delete -f "config/crd/bases"
+kubectl delete -f config/cluster-conf/e2e-namespace.yaml
